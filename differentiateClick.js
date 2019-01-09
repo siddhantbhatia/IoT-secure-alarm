05dbce5acf43a06f679fab6f23a2bb823fa9f9e7
cpan0001 <cpan0001@student.monash.edu>
2019-01-09 19:21:11 +0800

    refine differentiating different clicks

--- differentiateClick.js
+++ differentiateClick.js
@@ -1,11 +1,8 @@
 var b = require('bonescript');
 var clickCounter = 0;
+var firstclick = false;
 var prevState = 0;
-var hold1 = 0;
-var hold2 = 0;
-var hold3 = 0;
-
-var holdCounter = 0;
+var holdState = 0;
 
 b.pinMode('P8_19', b.INPUT);
 b.pinMode('P8_13', b.OUTPUT);
@@ -15,30 +12,30 @@ function check() {
   b.digitalRead('P8_19', checkButton);
 }
 
-function checkButton(x) {
-  if (clickCounter == 1 && hold1 > 10) {
+function time_end() {
+  if (clickCounter == 1 && holdState > 10){
     console.log("hold");
-  } else if (clickCounter == 1 && hold1 > 2) {
-    console.log("1");
-  } else if (clickCounter == 2 && hold2 > 2) {
-    console.log("2");
-  } else if (clickCounter == 3 && hold3 > 2) {
-    console.log("3");
+  } else {
+    console.log(clickCounter + " click(s)")
   }
+  firstclick = false;
+  clickCounter = 0;
+  prevState = 0;
+  holdState = 0;
+}
+
+function checkButton(x) {
   if(x.value == 1){
+    holdState += 1;
     b.digitalWrite('P8_13', b.HIGH);
-    if (prevState == 0) {
-      clickCounter += 1;
-    } else {
-      if (clickCounter == 1) {
-        hold1 += 1;
-      } else if (clickCounter == 2) {
-        hold2 += 1;
-      } else if (clickCounter == 3) {
-        hold3 += 1;
-      }
+    if (firstclick == false) {
+      setTimeout(time_end, 1500);
+      firstclick = true;
     }
-    prevState = 1;
+    if (prevState != 1){
+      clickCounter += 1;
+      prevState = 1;
+    }
   }
   else {
     b.digitalWrite('P8_13', b.LOW);
