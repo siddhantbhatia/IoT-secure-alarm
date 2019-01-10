5bb6901f471a061574e66a3475a23a1ce7aa7a42
unknown <nalw0001@student.monash.edu>
2019-01-10 13:36:59 +0800

    Changing the interval

--- buttonClick.js
+++ buttonClick.js
@@ -1,7 +1,7 @@
 var b = require('bonescript');
 b.pinMode('P8_19', b.INPUT);
 b.pinMode('P8_13', b.OUTPUT);
-setInterval(check,100);
+setInterval(check,40);
 
 function check(){
 b.digitalRead('P8_19', checkButton);
