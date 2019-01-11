9cce758d86745828478dfd0585297930e114259a
LeeYanHan <ylee0004@student.monash.edu>
2019-01-11 17:42:02 +0800

    will display how many clicks within 1.5s

--- buttonClick.js
+++ buttonClick.js
@@ -1,15 +1,28 @@
 var b = require('bonescript');
+var clicks=0;
+var clicked=false;
 b.pinMode('P8_19', b.INPUT);
 b.pinMode('P8_13', b.OUTPUT);
-setInterval(check,70);
+var timer=setInterval(check,100);
 
 function check(){
 b.digitalRead('P8_19', checkButton);
 }
 
+function button(){
+  console.log(clicks);
+  clicks=0;
+  clicked=false;
+}
+
 function checkButton(x) {
   if(x.value == 1){
     b.digitalWrite('P8_13', b.HIGH);
+    clicks+=1;
+    if (clicked==false){
+      setTimeout(button,1500);
+      clicked=true;
+    }
   }
   else{
     b.digitalWrite('P8_13', b.LOW);
