2d1efe5671b8e3bfa96a64fddd360eda338e1bcb
unknown <nalw0001@student.monash.edu>
2019-01-12 17:43:10 +0800

    recognizing different number of clicks

--- buttonClick.js
+++ buttonClick.js
@@ -1,17 +1,41 @@
 var b = require('bonescript');
+var clickCount = 0;
+var start = 0;
+var clicked = 0;
 b.pinMode('P8_19', b.INPUT);
 b.pinMode('P8_13', b.OUTPUT);
-setInterval(check,40);
+
+var timer = setInterval(check,80);
 
 function check(){
 b.digitalRead('P8_19', checkButton);
 }
 
-function checkButton(x) {
-  if(x.value == 1){
-    b.digitalWrite('P8_13', b.HIGH);
+function send_signal() {
+  console.log(clickCount);
+  reset_counts();
+}
+
+function reset_counts() {
+    start = 0;
+  clickCount = 0;
+  clicked = 0;
+}
+
+function checkButton(button) {
+  if(button.value == 1){
+    if (start == 0) {
+      setTimeout(send_signal, 2000);
+      start += 1;
+    }
+    if(clicked == 0){
+      b.digitalWrite('P8_13', b.HIGH);
+      clickCount += 1;
+      clicked = 1;
+    }
   }
-  else{
+  else {
     b.digitalWrite('P8_13', b.LOW);
+    clicked = 0;
   }
 }
