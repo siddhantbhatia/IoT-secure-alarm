e58fee71544e5ac93641d63809ae810aad7d63c8
Unknown <siddhant03b@gmail.com>
2019-01-19 23:12:16 +0800

    add server socket communication

--- main.js
+++ main.js
@@ -1,28 +1,56 @@
-var http = require("http"); // include http module
-var fs = require("fs");
-var path = require("path");
+// var http = require("http"); // include http module
+// var fs = require("fs");
+// var path = require("path");
+// var io = require("socket.io")();
 
-var app = http.createServer(function (req, res) { //create http server
-	console.log(req.url);
-//	if (req.url == "/") {
-		fs.readFile("index.html", function (err, data) {
-			res.setHeader("Content-Type", "text/html");
-			res.write(data);
-			res.end();
-		}); 
-//	} else if (req.url == "/styles.css") { // serving static css
-//		var cssPath = path.join(__dirname, req.url); // location of styles.css
-//		var fileStream = fs.createReadStream(cssPath, "UTF-8");
-//		res.writeHead(500, {"Content-Type": "text/css"});
-//		fileStream.pipe(res);
-//	}
-}).listen(8080);
+// var app = http.createServer(function(req, res) {
+//   //create http server
+//   //	if (req.url == "/") {
+//   fs.readFile("index.html", function(err, data) {
+//     res.setHeader("Content-Type", "text/html");
+//     res.write(data);
+//     res.end();
+//   });
+//   //	} else if (req.url == "/styles.css") { // serving static css
+//   //		var cssPath = path.join(__dirname, req.url); // location of styles.css
+//   //		var fileStream = fs.createReadStream(cssPath, "UTF-8");
+//   //		res.writeHead(500, {"Content-Type": "text/css"});
+//   //		fileStream.pipe(res);
+//   //	}
+// });
 
-// http://127.0.0.1:8080/
-//app.listen(8080);
+// io.on("connection", function(socket) {
+//   console.log("a user connected");
+// });
 
-function activateAlarm() {
-	document.getElementById("alarm").innerHTML = "ALARMMM!!!";
-	console.log("clicked");
-	// TODO: send signal to activate LED alarm;
-}
+// app.listen(3000, "0.0.0.0", function() {
+//   console.log("listening on *:3000");
+// });
+
+// function activateAlarm() {
+//   document.getElementById("alarm").innerHTML = "ALARMMM!!!";
+//   console.log("clicked");
+//   // TODO: send signal to activate LED alarm;
+// }
+
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+  socket.on("button clicked", function() {
+    console.log("clicked");
+  });
+});
+
+http.listen(3000, "0.0.0.0", function() {
+  console.log("listening on *:3000");
+});
