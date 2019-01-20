511b66f983931a6394e1b98ebb08fca95500d7ef
Unknown <siddhant03b@gmail.com>
2019-01-20 17:11:24 +0800

    added physical button response

--- main.js
+++ main.js
@@ -1,90 +1,55 @@
-// var http = require("http"); // include http module
-// var fs = require("fs");
-// var path = require("path");
-// var io = require("socket.io")();
+// --- Physical Button Click --- //
+// ground team
 
-// var app = http.createServer(function(req, res) {
-//   //create http server
-//   //	if (req.url == "/") {
-//   fs.readFile("index.html", function(err, data) {
-//     res.setHeader("Content-Type", "text/html");
-//     res.write(data);
-//     res.end();
-//   });
-//   //	} else if (req.url == "/styles.css") { // serving static css
-//   //		var cssPath = path.join(__dirname, req.url); // location of styles.css
-//   //		var fileStream = fs.createReadStream(cssPath, "UTF-8");
-//   //		res.writeHead(500, {"Content-Type": "text/css"});
-//   //		fileStream.pipe(res);
-//   //	}
-// });
+var b = require("bonescript");
+b.pinMode("P8_19", b.INPUT);
+b.pinMode("P8_13", b.OUTPUT);
 
-// io.on("connection", function(socket) {
-//   console.log("a user connected");
-// });
+// physical button click listener
+var timer = setInterval(check, 50);
 
-// app.listen(3000, "0.0.0.0", function() {
-//   console.log("listening on *:3000");
-// });
-
-// function activateAlarm() {
-//   document.getElementById("alarm").innerHTML = "ALARMMM!!!";
-//   console.log("clicked");
-//   // TODO: send signal to activate LED alarm;
-// }
-
-// var app = require("express")();
-// var http = require("http").Server(app);
-// var io = require("socket.io")(http);
-
-// button click code 
-var b = require('bonescript');
-b.pinMode('P8_19', b.INPUT);
-b.pinMode('P8_13', b.OUTPUT);
-
-function check(){
-  b.digitalRead('P8_19', checkButton);
-  
+function check() {
+  b.digitalRead("P8_19", checkButton);
 }
+
 function checkButton(x) {
-  if(x.value == 1){
-    b.digitalWrite('P8_13', b.HIGH);
-  }
-  else{
-    b.digitalWrite('P8_13', b.LOW);
+  if (x.value == 1) {
+    b.digitalWrite("P8_13", b.HIGH);
+  } else {
+    b.digitalWrite("P8_13", b.LOW);
   }
 }
 
-// for http module
+// --- Server --- //
 var http = require("http"); // include http module
 var fs = require("fs");
 var path = require("path");
 
-var app = http.createServer(function (req, res) { //create http server
-	console.log(req.url);
-	fs.readFile("index.html", function (err, data) {
-			res.setHeader("Content-Type", "text/html");
-			res.write(data);
-			res.end();
-		  
-		});
+var app = http.createServer(function(req, res) {
+  //create http server
+  console.log(req.url);
+  fs.readFile("index.html", function(err, data) {
+    res.setHeader("Content-Type", "text/html");
+    res.write(data);
+    res.end();
+  });
 });
 
-app.listen(8888);
+app.listen(8888); // webpage port 8888 --> http://beaglebone.local:8888/
 
-// socket.io connection
+// --- Webpage Button Click --- //
+// reponse team
 
-var io = require('socket.io').listen(app);
+// webpage message listener
+var io = require("socket.io").listen(app);
 
 io.on("connection", function(socket) {
   console.log("a user connected");
   socket.on("disconnect", function() {
     console.log("user disconnected");
   });
-  socket.on("button clicked", function() {
-      b.digitalWrite('P8_13', b.HIGH);
+  socket.on("rs-button-click", function() {
+    b.digitalWrite("P8_13", b.HIGH);
     console.log("clicked");
   });
 });
-
-
