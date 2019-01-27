837727a68554f4c73ba4603c707cc95f127b1c5a
LeeYanHan <ylee0004@student.monash.edu>
2019-01-27 18:17:42 +0800

    setting up performance test

---
+++ test/ping.js
@@ -0,0 +1,14 @@
+var Server = require("../Server/Server");
+var socket = require("socket.io");
+
+var myServer = new Server();
+var serverInstance = myServer.createServer();
+myServer.listen();
+
+var io = socket.listen(serverInstance);
+
+var count = 0, start_time;
+
+io.on('connection', function(socket) {
+    console.log('connected')
+})
